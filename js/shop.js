4b21801f9592598a9ee3bf44bac28f5586b13901
최형윤 <125331885+HyungYoon-Choi@users.noreply.github.com>
2023-03-13 17:50:47 +0900

    json 추가

--- js/shop.js
+++ js/shop.js
@@ -37,26 +37,42 @@
     // window.onload = function () {}
     
     // 슬라이드쇼
-    let slideIndex = 1;
-    showSlides(slideIndex);
+    let slideIndex = 0;
+    showSlides();
     
-    function showSlides(n){
+    function showSlides(){
         let i;
-        let slides = document.getElementsByClassName('img-slide');
-        if(n > slides.length){
-            slideIndex = 1
-        }
-        if(n < 1){
-            slideIndex = slides.length
-        }
+        const slides = document.getElementsByClassName('img-slide');
+        const dot = document.getElementsByClassName('dot')
         for(i = 0; i < slides.length; i++){
             slides[i].style.display = "none";
+            dot[i].classList.remove("active");
+        }
+        slideIndex++;
+        if(slideIndex > slides.length){
+            slideIndex = 1;
         }
         slides[slideIndex -1].style.display = "block";
+        dot[slideIndex -1].classList.add('active');
+        setTimeout(showSlides, 3000);
     }
 
 }());
+document.getElementsByClassName('tablinks')[0].click();
 
+function openBest(e, bid){
+    const tabcontent = document.getElementsByClassName('besttabcontent');
+    for(i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+    const tablinks = document.getElementsByClassName('tablinks');
+    for(i = 0; i < tablinks.length; i++){
+        tablinks[i].classList.remove('active');
+    }
+
+    document.getElementById(bid).style.display = 'block';
+    e.currentTarget.classList.add('active');
+}
 
 function viewTab(e) {
     const tabcontent = document.getElementsByClassName('tabcontent');
